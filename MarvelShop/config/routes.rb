b8477f3bafcb3bd3b911190833d2c925ac8a97ed
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-12-18 14:17:48 +0300

    removed the comments scaffold

--- MarvelShop/config/routes.rb
+++ MarvelShop/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :requests, :reviews
   resources :tshirts
-  resources :comments
   devise_for :users
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
